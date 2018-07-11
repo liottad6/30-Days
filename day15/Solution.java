aadd2fbf315f75ac3d6c96f3cc9ace94b950ad6d
David Liotta <liottad6@students.rowan.edu>
2018-07-11 16:00:23 -0400

    Adding day 15 and 17

---
+++ day15/Solution.java
@@ -0,0 +1,37 @@
+package day15;
+
+/**
+ * @author David Liotta
+ */
+
+import java.util.Scanner;
+
+public class Solution {
+	public static  Node insert(Node head,int data) {
+        //Complete this method
+		Node nNode = new Node(data);
+		head.next = nNode;
+		return head;
+    }
+
+	public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
---
+++ day17/Calculator.java
@@ -0,0 +1,15 @@
+package day17;
+
+/**
+ * @author David Liotta
+ *
+ */
+public class Calculator {
+
+	public int power(int n, int p) throws Exception {
+		if(n < 0 || p < 0) {
+			throw new Exception("n and p should be non-negative");
+		}else
+			return (int) Math.pow(n, p);
+	}	
+}
